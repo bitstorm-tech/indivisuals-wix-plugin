44583cdba4aeb25d5eed4d13fe7b3f7a68aea7f5
Josef Bauer <josef.bauer.1st@gmail.com>
2025-05-24 00:34:19 +0200

    Initial commit

---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/Button"
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+
+    <Head title="Welcome">
+    </Head>
+    <div class="p-12">
+        <Button>Button</Button>
+    </div>
+</template>
