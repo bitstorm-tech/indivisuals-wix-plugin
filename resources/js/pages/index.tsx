b7ac6b61d6c318ee1bb6effa65a4b838cccadb0d
Josef Bauer <josef.bauer.1st@gmail.com>
2025-06-01 20:22:10 +0200

    Add initial Prompt model

--- resources/js/pages/index.tsx
+++ resources/js/pages/index.tsx
@@ -1,25 +1,14 @@
 import ImagePicker from '@/components/image-picker';
 import { Head } from '@inertiajs/react';
-import { useState } from 'react';
 
 export default function Welcome() {
-    const [userId, setUserId] = useState('n/a');
-
-    window.addEventListener('message', (event) => {
-        setUserId(event.data.userId);
+    window.addEventListener('message', () => {
     });
 
     return (
         <>
-            <Head title="TheIndivisuals">
-                <link rel="preconnect" href="https://fonts.bunny.net" />
-                <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-            </Head>
+            <Head title="TheIndivisuals" />
             <div className="flex flex-col gap-4 p-12">
-                <button onClick={() => setUserId('n/a')} className="btn">
-                    Reset
-                </button>
-                <span>UserID: {userId}</span>
                 <ImagePicker />
             </div>
         </>
