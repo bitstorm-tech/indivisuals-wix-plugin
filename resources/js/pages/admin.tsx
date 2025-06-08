ddb6a50fb529e77db8d8bd3aa237022529c1ed40
Josef Bauer <josef.bauer.1st@gmail.com>
2025-06-08 22:35:35 +0200

    Refactor admin page: move sub-components to own folder

--- resources/js/pages/admin.tsx
+++ resources/js/pages/admin.tsx
@@ -1,9 +1,9 @@
-import AdminHeader from '@/components/admin-header';
-import CategoryFilter from '@/components/category-filter';
-import DeleteConfirmationDialog from '@/components/delete-confirmation-dialog';
-import NewPromptDialog from '@/components/new-prompt-dialog';
-import PromptTable from '@/components/prompt-table';
-import TestPromptDialog from '@/components/test-prompt-dialog';
+import AdminHeader from '@/components/admin/admin-header';
+import CategoryFilter from '@/components/admin/category-filter';
+import DeleteConfirmationDialog from '@/components/admin/delete-confirmation-dialog';
+import NewPromptDialog from '@/components/admin/new-prompt-dialog';
+import PromptTable from '@/components/admin/prompt-table';
+import TestPromptDialog from '@/components/admin/test-prompt-dialog';
 import { Head, router } from '@inertiajs/react';
 import { useEffect, useState } from 'react';
 
